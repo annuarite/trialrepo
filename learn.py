0ba4445bdd1ffe1522f8024c5043ea0090fdfcba
annuarite <annuaritenyaruai@gmail.com>
2024-04-26 17:51:17 +0300

    a list of a few git commands

--- learn.py
+++ learn.py
@@ -1,2 +1,13 @@
 print ("I am so anxious but i will get through this")
 print ("learning is the theme of the month")
+# GIT COMMANDS
+
+ # git init , git status,  git add . ,  git commit -m "message" 
+
+ # git push -u origin master (or whatever branch I'm on)
+
+ # git remote -v, git remote add origin https ......
+
+ # git branch name_of_new_branch,  git branch,  git checkout 
+
+ # ls, ls -la,
